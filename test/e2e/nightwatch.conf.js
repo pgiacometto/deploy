316c19156b8c06029ae5faf8518894d2daed7578
pedro giacometto <pedro.giacometto@gmail.com>
2017-12-19 00:37:02 -0500

    Add circle.yml

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -10,7 +10,7 @@ module.exports = {
   selenium: {
     start_process: true,
     server_path: require('selenium-server').path,
-    host: 'localhost',
+    host: '127.0.0.1',
     port: 4444,
     cli_args: {
       'webdriver.chrome.driver': require('chromedriver').path
